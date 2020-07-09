e51da15dec9f10b18d0c75c0d8e2b575ac1c97d1
cly4auHo <arsen.dmitriev96@gmail.com>
2020-07-09 19:04:43 +0300

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Explorer
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PathField = new System.Windows.Forms.TextBox();
+            this.ButtonOpen = new System.Windows.Forms.Button();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.NewNameField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path: ";
+            // 
+            // PathField
+            // 
+            this.PathField.Location = new System.Drawing.Point(53, 6);
+            this.PathField.Name = "PathField";
+            this.PathField.ReadOnly = true;
+            this.PathField.Size = new System.Drawing.Size(654, 20);
+            this.PathField.TabIndex = 1;
+            // 
+            // ButtonOpen
+            // 
+            this.ButtonOpen.Location = new System.Drawing.Point(713, 4);
+            this.ButtonOpen.Name = "ButtonOpen";
+            this.ButtonOpen.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOpen.TabIndex = 2;
+            this.ButtonOpen.Text = "Open";
+            this.ButtonOpen.UseVisualStyleBackColor = true;
+            this.ButtonOpen.Click += new System.EventHandler(this.ButtonOpen_Click);
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(15, 59);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(773, 379);
+            this.treeView.TabIndex = 3;
+            this.treeView.KeyUp += new System.Windows.Forms.KeyEventHandler(this.treeView_KeyUp);
+            this.treeView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView_MouseDoubleClick);
+            // 
+            // NewNameField
+            // 
+            this.NewNameField.Location = new System.Drawing.Point(15, 33);
+            this.NewNameField.Name = "NewNameField";
+            this.NewNameField.Size = new System.Drawing.Size(773, 20);
+            this.NewNameField.TabIndex = 4;
+            this.NewNameField.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NewNameField_MouseClick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NewNameField);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.ButtonOpen);
+            this.Controls.Add(this.PathField);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form";
+            this.Text = "File Explorer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PathField;
+        private System.Windows.Forms.Button ButtonOpen;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.TextBox NewNameField;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Explorer
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private const string message = "New name here (without: /\\:*?«<>|)";
+        private char[] wrongCharactersForNameFile = { '/', '\\', ':', '*', '?', '«', '<', '>', '|' };
+
+        public Form()
+        {
+            InitializeComponent();
+            NewNameField.Text = message;
+        }
+
+        private void treeView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void treeView_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                OpenFile();
+            else if (e.KeyCode == Keys.Delete)
+                DeleteFile();
+            else if (e.KeyCode == Keys.F2)
+                RenameFile();
+        }
+
+        private void ButtonOpen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select path" };
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {           
+                PathField.Text = fbd.SelectedPath;
+                BuildTree(fbd.SelectedPath);
+            }
+        }
+
+        private void NewNameField_MouseClick(object sender, MouseEventArgs e)
+        {
+            NewNameField.Text = "";
+        }
+
+        private void BuildTree(string path)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            if (directoryInfo.Exists)
+            {
+                treeView.Nodes.Clear();
+                BuildTree(directoryInfo, treeView.Nodes);
+                //Вuild_tree_oly_o_evet,_forexampleifyouserwotclick_oH_it,_it_wottвuild_tree
+            }
+        }
+
+        private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+        {
+            TreeNode curNode = addInMe.Add(directoryInfo.Name);
+            curNode.Tag = directoryInfo.FullName;
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                TreeNode node = curNode.Nodes.Add(file.FullName, file.Name);
+                node.Tag = file.FullName;
+            }
+
+            foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+                BuildTree(subdir, curNode.Nodes);
+        }
+
+        private void OpenFile()
+        {
+            if (treeView.SelectedNode != null)
+            {
+                string path = treeView.SelectedNode.Tag as string;
+
+                if (!string.IsNullOrEmpty(path))
+                    System.Diagnostics.Process.Start(path);
+            }
+        }
+
+        private void RenameFile()
+        {
+            if (treeView.SelectedNode != null && !string.IsNullOrEmpty(NewNameField.Text.Trim()))
+            {
+                foreach (char item in wrongCharactersForNameFile)
+                {
+                    if (NewNameField.Text.Contains(item))
+                    {
+                        NewNameField.Text = message;
+                        return;
+                    }
+                }
+
+                string oldPath = treeView.SelectedNode.Tag as string;
+
+                if (!string.IsNullOrEmpty(oldPath))
+                {
+                    FileAttributes fileAttributes = File.GetAttributes(oldPath);
+
+                    if (fileAttributes.HasFlag(FileAttributes.Directory))
+                    {
+                        if (Directory.Exists(oldPath))
+                        {
+                            string parentDirectory = Directory.GetParent(oldPath).FullName;
+                            string newPath = parentDirectory + "\\" + NewNameField.Text.Trim();
+
+                            if (Directory.Exists(newPath))
+                            {
+                                NewNameField.Text = "Directory is exist";
+                                return;
+                            }
+                            else
+                            {
+                                if (treeView.SelectedNode.Parent == null)
+                                    PathField.Text = newPath; 
+
+                                treeView.SelectedNode.Text = NewNameField.Text.Trim();
+                                Directory.Move(oldPath, newPath);
+                                BuildTree(PathField.Text);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (File.Exists(oldPath))
+                        {
+                            string fileDirectory = Path.GetDirectoryName(oldPath);
+                            string ext = Path.GetExtension(oldPath);
+                            string newPath = fileDirectory + "\\" + NewNameField.Text.Trim() + ext;
+
+                            if (File.Exists(newPath))
+                            {
+                                NewNameField.Text = "File is exist";
+                                return;
+                            }
+                            else
+                            {
+                                treeView.SelectedNode.Text = NewNameField.Text.Trim() + ext;
+                                File.Move(oldPath, newPath);
+                                BuildTree(PathField.Text);
+                            }
+                        }
+                    }
+                }
+
+                NewNameField.Text = message;
+            }
+        }
+
+        private void DeleteFile()
+        {
+            if (treeView.SelectedNode != null)
+            {
+                string path = treeView.SelectedNode.Tag as string;
+
+                if (!string.IsNullOrEmpty(path))
+                {
+                    FileAttributes fileAttributes = File.GetAttributes(path);
+
+                    if (fileAttributes.HasFlag(FileAttributes.Directory))
+                    {
+                        if (Directory.Exists(path))
+                        {
+                            if (treeView.SelectedNode.Parent == null)
+                                treeView.Nodes.Remove(treeView.SelectedNode);
+                            else
+                                treeView.SelectedNode.Parent.Nodes.Remove(treeView.SelectedNode);
+
+                            Directory.Delete(path, true);
+                        }
+                    }
+                    else
+                    {
+                        if (File.Exists(path))
+                        {
+                            if (treeView.SelectedNode.Parent == null)
+                                treeView.Nodes.Remove(treeView.SelectedNode);
+                            else
+                                treeView.SelectedNode.Parent.Nodes.Remove(treeView.SelectedNode);
+
+                            File.Delete(path);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
